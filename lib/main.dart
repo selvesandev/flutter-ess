bcc1ae17bec8f285fc94ef03433487b183930acd
selvesan <selvesan@selvesans-MacBook-Pro.local>
2019-09-09 22:57:43 +0545

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  build(context) {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Flutter Ecommerce'),),
+        appBar: AppBar(
+          title: Text('Flutter Ecommerce'),
+        ),
       ),
     );
   }
